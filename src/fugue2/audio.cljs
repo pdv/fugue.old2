d474b6046e0ac5dd1a8efbf1f41b275f6c66fc27
pdv <philipjdelvecchio@gmail.com>
2018-03-23 10:56:11 -0400

    Making sound (fix)

---
+++ src/fugue2/audio.cljs
@@ -0,0 +1,48 @@
+(ns fugue2.audio
+  (:require [fugue2.engine :as engine]))
+
+(defonce ctx (atom nil))
+
+(defn init-audio! []
+  (reset! ctx (engine/make-ctx)))
+
+(defn reset-audio! []
+  (engine/close! @ctx)
+  (init-audio!))
+
+(defn out [in]
+  (engine/out @ctx in)
+  in)
+
+;;; Mix
+
+(defn gain
+  "Multiplies the amplitude of in by amp"
+  [in amp]
+  (engine/gain @ctx in amp))
+
+;;; Oscillators
+
+(defn sin-osc
+  "Starts a sine wave oscillator at the given frequency"
+  ([freq] (sin-osc freq 0))
+  ([freq detune]
+   (engine/oscillator @ctx :sine freq detune)))
+
+(defn saw
+  "Starts a saw wave oscillator at the given frequency"
+  ([freq] (saw freq 0))
+  ([freq detune]
+   (engine/oscillator @ctx :sawtooth freq detune)))
+
+(defn square
+  "Starts a square wave oscillator at the given frequency"
+  ([freq] (square freq 0))
+  ([freq detune]
+   (engine/oscillator @ctx :square freq detune)))
+
+(defn tri
+  "Starts a triangle wave oscillator at the given frequency"
+  ([freq] (tri freq 0))
+  ([freq detune]
+   (engine/oscillator @ctx :triangle freq detune)))
---
+++ src/fugue2/engine.cljs
@@ -0,0 +1,57 @@
+(ns fugue2.engine)
+
+(defn make-ctx [] (js/AudioContext.))
+
+(defn current-time [ctx]
+  (.-currentTime ctx))
+
+(defn sample-rate [ctx]
+  (.-sampleRate ctx))
+
+(defn resume! [ctx]
+  (.resume ctx))
+
+(defn suspend! [ctx]
+  (.suspend ctx))
+
+(defn close! [ctx]
+  (.close ctx))
+
+(defn out [ctx in]
+  (.connect in (.-destination ctx))
+  in)
+
+(def node? (partial instance? js/AudioNode))
+
+(defn attach-param! [modulator param]
+  (cond
+    (number? modulator) (set! (.-value param) modulator)
+    (node? modulator) (.connect modulator param)
+    :else (throw (js/Error. "Invalid modulator type"))))
+
+;;; Nodes
+
+(defn oscillator
+  [ctx type freq detune]
+  (let [osc-node (.createOscillator ctx)]
+    (set! (.-type osc-node) (clj->js type))
+    (attach-param! freq (.-frequency osc-node))
+    (attach-param! detune (.-detune osc-node))
+    (.start osc-node)
+    osc-node))
+
+(defn biquad-filter
+  ([ctx in type freq q]
+   (let [filter-node (.createBiquadFilter ctx)]
+     (set! (.-type filter-node) (clj->js type))
+     (attach-param! freq (.-frequency filter-node))
+     (attach-param! q (.-Q filter-node))
+     (.connect in filter-node)
+     filter-node)))
+
+(defn gain
+  [ctx in amount]
+  (let [gain-node (.createGain ctx)]
+    (attach-param! amount (.-gain gain-node))
+    (.connect in gain-node)
+    gain-node))
