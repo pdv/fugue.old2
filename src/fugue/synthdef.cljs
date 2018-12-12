47a90b6deadc695a640b3159965d3a4ead65c4cd
pdv <philipjdelvecchio@gmail.com>
2018-12-11 19:14:53 -0500

    Tests

--- src/fugue/synthdef.cljs
+++ src/fugue/synthdef.cljs
@@ -33,7 +33,6 @@
     (-> synthdef
         (update-in [:nodes node-id :audio-params param-name] conj modulator))))
 
-
 (defn- process-param
   [synthdef node-id param-name modulators]
   (reduce (fn [synthdef modulator]
@@ -44,8 +43,7 @@
 (defn- add-node
   "Returns a new synthdef with nodedef added"
   [synthdef id nodedef]
-  (let [id (random-uuid)
-        params (:audio-params nodedef)
+  (let [params (:audio-params nodedef)
         node-without-aparams (assoc nodedef :audio-params [])
         synthdef (update synthdef :nodes assoc id node-without-aparams)]
     (reduce (fn [synthdef [param-name modulators]]
--- test/fugue/synthdef_test.cljs
+++ test/fugue/synthdef_test.cljs
@@ -2,6 +2,13 @@
   (:require [fugue.synthdef :as sut]
             [cljs.test :as t :refer-macros [deftest]]))
 
-(deftest test-synthdef
-  (t/is false))
+(deftest test-source
+  (let [src (sut/source {:constructor "createOscillator"})
+        id (:output-id src)]
+    (print src)
+    (t/is (= #{id} (:source-ids src)))
+    (t/is (empty? (:connections src)))
+    (t/is (= {:constructor "createOscillator",
+              :audio-params []}
+             ((:nodes src) id)))))
 
